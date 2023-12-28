ea953583746f4bf9e0577740aa3adc64ed828364
Andrey <Andrey@Karmanov.ca>
2023-12-27 23:18:17 -0500

    delete things that shouldn't be tracked!

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+class YourAppTests(TestCase):
+    def test_example(self):
+        # Write your test code here
+        self.assertEqual(2 + 2, 4)
+
+    def test_another_example(self):
+        # Write your test code here
+        self.assertTrue(True)
+
+    def test_additional_example(self):
+        # Write your test code here
+        self.assertEqual("hello".upper(), "HELLO")
